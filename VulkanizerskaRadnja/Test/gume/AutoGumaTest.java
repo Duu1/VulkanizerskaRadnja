9169f9693e28b9205f6ed6bdcb382b8e55c95634
Duu1 <r.dusan97@gmail.com>
2018-03-21 16:13:13 +0100

    Testiranje.
    Izvrseno je testiranje klase AutoGuma, otkrivene i zatim ispravljene
    neke greske u metodama te klase.

--- VulkanizerskaRadnja/Test/gume/AutoGumaTest.java
+++ VulkanizerskaRadnja/Test/gume/AutoGumaTest.java
@@ -17,6 +17,7 @@ import org.junit.Test;
  */
 public class AutoGumaTest {
 
+	private AutoGuma a;
 	/**
 	 * @throws java.lang.Exception
 	 */
@@ -36,6 +37,7 @@ public class AutoGumaTest {
 	 */
 	@Before
 	public void setUp() throws Exception {
+		a = new AutoGuma();
 	}
 
 	/**
@@ -43,11 +45,116 @@ public class AutoGumaTest {
 	 */
 	@After
 	public void tearDown() throws Exception {
+		a = null;
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void setMarkaModel() {
+		a.setMarkaModel("Tigar");
+		
+		assertEquals("Tigar",a.getMarkaModel());
 	}
-
+	
+	@Test (expected = java.lang.RuntimeException.class)
+	public void setMarkaModelNull() {
+		a.setMarkaModel(null);
+	}
+	
+	@Test (expected = java.lang.RuntimeException.class)
+	public void setMarkaModelKratak() {
+		a.setMarkaModel("pr");
+	}
+	
+	@Test
+	public void testSetPrecnik() {
+		a.setPrecnik(20);
+		
+		assertEquals(20, a.getPrecnik());
+	}
+	
+	@Test (expected = java.lang.RuntimeException.class)
+	public void testSetPrecnikMali() {
+		a.setPrecnik(1);
+	}
+	
+	@Test (expected = java.lang.RuntimeException.class)
+	public void testSetPrecnikVeliki() {
+		a.setPrecnik(55);
+	}
+	
+	@Test
+	public void testSetSirina() {
+		a.setSirina(150);
+		
+		assertEquals(150, a.getSirina());
+	}
+	
+	@Test (expected = java.lang.RuntimeException.class)
+	public void testSetSirinaMala() {
+		a.setSirina(1);
+	}
+	
+	@Test (expected = java.lang.RuntimeException.class)
+	public void testSetSirinaVelika() {
+		a.setSirina(455);
+	}
+	
+	@Test
+	public void testSetVisina() {
+		a.setVisina(50);
+		
+		assertEquals(50, a.getVisina());
+	}
+	
+	@Test (expected = java.lang.RuntimeException.class)
+	public void testSetVisinaMala() {
+		a.setVisina(1);
+	}
+	
+	@Test (expected = java.lang.RuntimeException.class)
+	public void testSetVisinaVelika() {
+		a.setVisina(455);
+	}
+	
+	@Test
+	public void testToString() {
+		a.setMarkaModel("Tigar");
+		a.setPrecnik(20);
+		a.setSirina(150);
+		a.setVisina(80);
+		
+		assertEquals("AutoGuma [markaModel=Tigar, precnik=20, sirina=150, visina=80]",a.toString());
+	}
+	
+	@Test
+	public void testEqualsTrue() {
+		a.setMarkaModel("Tigar");
+		a.setPrecnik(20);
+		a.setSirina(150);
+		a.setVisina(80);
+		
+		AutoGuma a2 = new AutoGuma();
+		a2.setMarkaModel("Tigar");
+		a2.setPrecnik(20);
+		a2.setSirina(150);
+		a2.setVisina(80);
+		
+		assertTrue(a.equals(a2));
+	}
+	
+	@Test
+	public void testEqualsFalse() {
+		a.setMarkaModel("Tigar");
+		a.setPrecnik(20);
+		a.setSirina(150);
+		a.setVisina(80);
+		
+		AutoGuma a2 = new AutoGuma();
+		a2.setMarkaModel("Tigar");
+		a2.setPrecnik(20);
+		a2.setSirina(180);
+		a2.setVisina(80);
+		
+		assertFalse(a.equals(a2));
+	}
 }
